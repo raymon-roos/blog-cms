a00cfcb0337bd4f53ebf20129eb43cbb61420c6d
Raymon Roos <raymon.roos@hotmail.com>
2022-10-24 16:10:11 +0200

    refactor: query wrapper passing along DB conn

--- queries.php
+++ queries.php
@@ -4,11 +4,16 @@ declare(strict_types=1);
 
 require_once('connection.php');
 
-function findAllPosts(): array | false
+function findAllPosts($pdo): array | false
+{
+	return $pdo->query('SELECT * FROM `posts`;')->fetchAll() ?: false;
+}
+
+function query(callable $queryfunc): mixed
 {
 	try {
-		return connectDB()->query('SELECT * FROM `posts`;')->fetchAll() ?: false;
-	} catch (Throwable) {
+		return $queryfunc(connectDB());
+	} catch (\Throwable) {
 		return false;
 	}
 }
