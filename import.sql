4348e8d48ccbfe44700ddd45a79afb9701397456
Raymon Roos <raymon.roos@hotmail.com>
2022-11-09 20:53:44 +0100

    fix(git): revert temp commit
    This reverts commit d1dbd5fe62c676f7cdf896528dae686c589791d5.

--- import.sql
+++ import.sql
@@ -8,6 +8,7 @@ CREATE TABLE authors (
 	`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`name` ENUM('Miljuschka', 'Mounir Toub', 'Wim Ballieu') NOT NULL
 );
+
 INSERT INTO authors (`name`)
 VALUES ('Miljuschka'), ('Mounir Toub'), ('Wim Ballieu');
 
@@ -27,16 +28,18 @@ CREATE TABLE posts (
 	`likes` INT NOT NULL DEFAULT 0,
 	`content` TEXT NOT NULL
 );
+
 INSERT INTO posts (`title`, `date`, `img_url`, `author_id`, `likes`, `content`)
 VALUES (
 	'Pindakaas',
 	'2020:06:18 13:25:00',
 	'https://i.ibb.co/C0Lb7R1/pindakaas.jpg',
 	3,
-	10,
+	1,
 	'Verwarm de oven voor op 180 °C. Verdeel de pinda’s over een met bakpapier beklede bakplaat en rooster in ca. 8 min. lichtbruin. Schep regelmatig om. Maal de warme pinda’s in de keukenmachine in 4 min. tot een grove, dikke pindakaas. Schep de rand van de kom regelmatig schoon met een spatel. Voeg het zout, de olie en honing toe en maal nog 1 min. tot een gladde pindakaas. Schep in een pot en sluit af.
 	variatietip: Houd je van pindakaas met een smaakje? Voeg dan na de honing 1 el sambal badjak, 1 tl gemalen kaneel of 1 el fijngehakte pure chocolade toe. bewaartip: Je kunt de pindakaas 3 weken in de koelkast bewaren.'
-), (
+),
+(
 	'Baklava',
 	'2020:03:11 10:28:00',
 	'https://i.ibb.co/ZWVRdPT/baklava.jpg',
@@ -52,18 +55,4 @@ VALUES (
 	Bestrijk een vel baklavadeeg met gesmolten roomboter. Leg er een tweede vel op en bestrijk dat ook. Neem één portie van het walnotenmengsel en verdeel dat onderaan over het baklavadeeg. Rol op tot een staaf, leg deze in de bakvorm en bestrijk met gesmolten roomboter. Maak de rest van de staven op dezelfde manier.
 	Snijd elke staaf met een scherp mes meteen in zessen. Bak de baklava in circa 25 minuten goudbruin en krokant in de oven.
 	Neem de bakvorm uit de oven en verdeel de warme honing over de baklava. Garneer meteen met de rest van de fijngemalen walnoten. Laat de baklava minimaal 3 uur afkoelen voordat je ervan gaat genieten.'
-), (
-	'test',
-	'2022:10:28 17:23:00',
-	'test',
-	1,
-	11,
-	'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.'
-);
-
-CREATE TABLE posts_tags (
-	`post_id` INT NOT NULL,
-	`tag_id` INT NOT NULL,
-	FOREIGN KEY(`post_id`) REFERENCES `posts`(`id`),
-	FOREIGN KEY(`tag_id`) REFERENCES `tags`(`id`)
-);
+)
